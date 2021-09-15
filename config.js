81243cd9cb0e10ddf28f8fbf0b786c3e01725e12
Anteo Fabris <anteo8fabris@gmail.com>
2021-09-15 16:22:43 -0400

    Update config.js
    small fix

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 require("dotenv")
 
-const SECRET_KEY = process.env.SECRET_KEY || '9a2dea1e-bb7c-4a8d-ba45-2d089fe816c6'
+const SECRET_KEY = process.env.SECRET_KEY
 
 const PORT = process.env.PORT || 3000
 
